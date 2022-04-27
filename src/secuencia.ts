f4245cc3582729bb2ae901ab0f4329fd03d36d89
SilvioAlvarez67 <104239950+SilvioAlvarez67@users.noreply.github.com>
2022-04-26 22:33:13 -0300

    Initial commit

---
+++ src/secuencia.ts
@@ -0,0 +1,13 @@
+let pcardinal: string; 
+pcardinal = "este"; 
+let señal: string; 
+señal = "algoritmo";
+let ser: string; 
+ser = "es";
+let noser: string; 
+noser = "secuencial";
+
+console.log(pcardinal);
+console.log(señal);
+console.log(ser);
+console.log(noser);
